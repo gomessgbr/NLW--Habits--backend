dc8f9dbe37fabf28ee9b2a6c2d8e2fab673f5523
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-18 09:55:36 -0300

    feat: new routes get day and post habits

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,71 @@
-import {FastifyInstance}  from 'fastify'
+import dayjs from "dayjs";
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
 
 import { prisma } from "./lib/prisma";
 
+export async function appRoutes(app: FastifyInstance) {
+  app.post("/habits", async (request) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
+    const { title, weekDays } = createHabitBody.parse(request.body);
 
+    const totay = dayjs().startOf("day").toDate();
 
-export async function appRoutes(app: FastifyInstance){
-    app.get("/hello", async () => {
-        const habits = await prisma.habit.findMany();
-      
-        return habits;
-      });   
+    await prisma.habit.create({
+      data: {
+        title,
+        create_at: totay,
+        weekDays: {
+          create: weekDays.map((weekDay) => {
+            return {
+              week_day: weekDay,
+            };
+          }),
+        },
+      },
+    });
+  });
 
+  app.get("/day", async (request) => {
+    const getDayParams = z.object({
+      date: z.coerce.date(),
+    });
+
+    const { date } = getDayParams.parse(request.query);
+    const parsedDate = dayjs(date).startOf("day");
+    const weekDay = parsedDate.get("day");
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        create_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay,
+          },
+        },
+      },
+    });
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate(),
+      },
+      include: {
+        dayHabits: true,
+      },
+    });
+    const completedHabits = day?.dayHabits.map(dayHabit=>{
+      return dayHabit.habit_id
+    })
+
+    return {
+      possibleHabits,
+      completedHabits
+    };
+  });
 }
-
