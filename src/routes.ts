f13e69462e6141dbc8410ac3a699ca923562ad2e
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-21 12:12:03 -0300

    feat: return blanket array

--- src/routes.ts
+++ src/routes.ts
@@ -61,7 +61,9 @@ export async function appRoutes(app: FastifyInstance) {
     });
     const completedHabits = day?.dayHabits.map((dayHabit) => {
       return dayHabit.habit_id;
-    });
+    })  ?? []
+
+
 
     return {
       possibleHabits,
